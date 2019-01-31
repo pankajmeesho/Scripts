d4368d9b651b4f0ab379612d7db28a087a01eda9
Pankaj <43433104+pankajmeesho@users.noreply.github.com>
2019-01-31 14:06:02 +0530

    Update check_supervisor_process.sh
    update script

--- check_supervisor_process.sh
+++ check_supervisor_process.sh
@@ -5,6 +5,8 @@
 # location    : /usr/local/bin/scripts/
 # info       : this script is used to check whether all worker processes are running or not
 
+
+
 total_processes=$(sudo supervisorctl status|wc -l)
 running_processes=$(sudo supervisorctl status|grep 'RUNNING'|wc -l)
 
@@ -12,6 +14,14 @@ if [ $total_processes -eq $running_processes ]; then
 echo "OK: All the Prcoesses are running"
 exit 0
 else
-echo "Critical: All Prcoesses are not running"
+sudo supervisorctl status|awk '{print $1}' > /tmp/process_list
+	while read line
+	do
+
+		count=$(sudo supervisorctl status|grep "$line"|grep -i 'RUNNING'|wc -l)
+		if [ $count -eq 0 ]; then
+			echo "$line is not running"
+		fi
+	done < /tmp/process_list
 exit 2
 fi
